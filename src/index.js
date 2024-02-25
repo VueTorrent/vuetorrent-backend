880dfd08312e4641ddb68c58361c6db4d0cff72d
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-02-25 11:35:16 +0100

    Add ping route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import cors from 'cors'
 import express from 'express'
-import configRoutes from './routes/config.js'
+import configRouter from './routers/config.js'
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
 app.use(cors())
-app.use('/config', configRoutes)
+app.use('/config', configRouter)
+
+app.get('/ping', async (req, res) => {
+  res.send('pong')
+})
 
 async function handle_signal(signal) {
   console.log(`Received ${ signal } signal. Gracefully shutting down...`)
