4ce415c04b38e2d4128558661c533b1e11553171
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-02-25 10:39:59 +0100

    Handle data file doesn't exists

--- server.js
+++ server.js
@@ -7,10 +7,29 @@ const CONFIG_FILE_PATH = 'data/data.json'
 
 app.use(express.json())
 
+async function getConfig() {
+  const canAccess = await fs.access(CONFIG_FILE_PATH, fs.constants.F_OK)
+  .then(() => true, () => false)
+
+  if (!canAccess) {
+    // File doesn't exists
+    return {}
+  }
+
+  const canEdit = await fs.access(CONFIG_FILE_PATH, fs.constants.R_OK | fs.constants.W_OK)
+  .then(() => true, () => false)
+
+  if (!canEdit) {
+    // File not readable / writable
+    throw new Error("Data file isn't readable / writable")
+  }
+
+  return JSON.parse(await fs.readFile(CONFIG_FILE_PATH, 'utf8'))
+}
+
 app.get('/config', async (req, res) => {
   try {
-    const configData = await fs.readFile(CONFIG_FILE_PATH, 'utf8')
-    const config = JSON.parse(configData)
+    const config = await getConfig()
     res.json(config)
   } catch (err) {
     res.status(500).json({ error: err.message })
@@ -20,9 +39,8 @@ app.get('/config', async (req, res) => {
 app.get('/config/:key', async (req, res) => {
   const { key } = req.params
   try {
-    const configData = await fs.readFile(CONFIG_FILE_PATH, 'utf8')
-    const config = JSON.parse(configData)
-    if (config.hasOwnProperty(key)) {
+    const config = await getConfig()
+    if (Object.hasOwn(config, key)) {
       res.json({ [key]: config[key] })
     } else {
       res.status(404).json({ error: 'Key not found' })
@@ -36,8 +54,7 @@ app.put('/config/:key', async (req, res) => {
   const { key } = req.params
   const { value } = req.body
   try {
-    let configData = await fs.readFile(CONFIG_FILE_PATH, 'utf8')
-    let config = JSON.parse(configData)
+    const config = await getConfig()
     config[key] = value
     await fs.writeFile(CONFIG_FILE_PATH, JSON.stringify(config, null, 2))
     res.json({ message: 'Config updated successfully' })
