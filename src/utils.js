d32d108aba2c080f7ea14660c899fb43e09866b2
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-02-26 20:44:53 +0100

    Handle concurrent requests

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,25 @@ import { promises as fs } from 'fs'
 
 const DATA_FILE_PATH = 'data/data.json'
 
-async function getData() {
+const queue = []
+
+async function processQueue() {
+  if (queue.length === 0) return
+
+  const { data, resolve, reject } = queue[0]
+
+  try {
+    await fs.writeFile(DATA_FILE_PATH, JSON.stringify(data, null, 2))
+    resolve()
+  } catch (err) {
+    reject(err)
+  } finally {
+    queue.shift()
+    await processQueue()
+  }
+}
+
+export async function getData() {
   const canAccess = await fs.access(DATA_FILE_PATH, fs.constants.F_OK)
   .then(() => true, () => false)
 
@@ -22,11 +40,11 @@ async function getData() {
   return JSON.parse(await fs.readFile(DATA_FILE_PATH, 'utf8'))
 }
 
-async function setData(data) {
-  return await fs.writeFile(DATA_FILE_PATH, JSON.stringify(data, null, 2))
-}
-
-export {
-  getData,
-  setData
+export async function setData(data) {
+  return new Promise((resolve, reject) => {
+    queue.push({ data, resolve, reject })
+    if (queue.length === 1) {
+      processQueue()
+    }
+  })
 }
