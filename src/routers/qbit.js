181ddb5d0e4cdab1a8e23bd3da5cb49e9c07695f
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2024-08-25 15:06:45 -0500

    Add insecure SSL certificate  handling

--- src/routers/qbit.js
+++ src/routers/qbit.js
@@ -3,13 +3,12 @@ import httpProxy from 'http-proxy'
 
 const router = express.Router()
 
-const proxy = httpProxy.createProxyServer({
-  host: process.env.QBIT_BASE,
-  xfwd: true,
-  secure: true,
-})
-
 router.use((req, res, next) => {
+  const proxy = httpProxy.createProxyServer({
+    host: process.env.QBIT_BASE,
+    xfwd: true,
+    secure: process.env.USE_INSECURE_SSL !== 'true',
+  })
   proxy.web(req, res, { target: `${process.env.QBIT_BASE}/api` }, next)
 })
 
