4028fe83966b1b8ae0c324761c024efe700864ab
Rémi Marseault <larsluph@larsluph.dev>
2024-10-06 09:12:47 -0500

    chore: Improve error readability

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -19,12 +19,20 @@ async function isValidSID(SID) {
   if (SID) {
     headers['Cookie'] = `SID=${ SID }`
   }
-  
+
   try {
-    const r = await axios.get(`${process.env.QBIT_BASE}/api/v2/app/version`, { headers })
+    const r = await axios.get(`${ process.env.QBIT_BASE }/api/v2/app/version`, { headers })
     return r.status === 200
   } catch (error) {
-    console.error('Error occurred while fetching version:', error)
+    console.error('Error occurred while fetching version:')
+    if (error.name === 'AggregateError') {
+      console.error(error.errors.map(err => err.message).join('\n'))
+    } else if (error.response) {
+      console.error(`[${error.response.status}] ${error.response.data}`)
+    } else {
+      console.error(error.name, error.message)
+    }
+
     return false
   }
 }
