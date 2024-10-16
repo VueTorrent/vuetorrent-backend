50150675b03a9efbb4672adde295f512a90c3bc1
Rémi Marseault <larsluph@larsluph.dev>
2024-10-16 15:41:23 +0200

    temp(#62): Add qbit headers logging

--- src/routers/qbit/index.js
+++ src/routers/qbit/index.js
@@ -4,12 +4,19 @@ import httpProxy from 'http-proxy'
 const router = Router()
 
 router.use((req, res, next) => {
+  console.log('Incoming headers:', req.headers)
+
   const proxy = httpProxy.createProxyServer({
     host: process.env.QBIT_BASE,
     xfwd: true,
     secure: process.env.USE_INSECURE_SSL !== 'true',
   })
-  proxy.web(req, res, { target: `${process.env.QBIT_BASE}/api` }, next)
+
+  proxy.on('proxyReq', proxyReq => {
+    console.log('Forwarding headers:', proxyReq.getHeaders())
+  })
+
+  proxy.web(req, res, { target: `${ process.env.QBIT_BASE }/api` }, next)
 })
 
 export default router
