66299603437915a036d08c63451d2a306ec49a55
Rémi Marseault <larsluph@larsluph.dev>
2025-05-24 18:38:58 +0000

    feat(https): Handle HTTPS cert and key

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { config } from 'dotenv'
 import express, { Router } from 'express'
+import https from 'https'
 import morgan from 'morgan'
 import scheduler from 'node-schedule'
 import authMiddleware from './middlewares/auth.js'
 import configRouter from './routers/config/index.js'
 import qbitRouter from './routers/qbit/index.js'
 import { checkForUpdate } from './routines/update.js'
+import { getSSLOptionsFromEnv } from './ssl.js'
 import { assert_env } from './utils.js'
 
 config()
@@ -49,22 +51,34 @@ app.use(async (req, res) => {
   res.status(404).send('404 Not Found')
 })
 
-const server = app.listen(PORT, async () => {
-  console.log('Checking for updates...')
-  console.log(await checkForUpdate())
-  console.log(`Server is running on port ${ PORT }`)
+function launchServer(serverInstance, protocol) {
+  serverInstance.listen(PORT, async () => {
+    console.log('Checking for updates...')
+    console.log(await checkForUpdate())
+    console.log(`${protocol} server is running on port ${PORT}`)
+    const update_cron = process.env.UPDATE_VT_CRON
+    if (update_cron && update_cron.length) {
+      scheduler.scheduleJob(
+        'Update VueTorrent',
+        update_cron,
+        async () => {
+          return await checkForUpdate()
+        }
+      );
+    }
+  })
+}
 
-  const update_cron = process.env.UPDATE_VT_CRON
-  if (update_cron && update_cron.length) {
-    scheduler.scheduleJob(
-      'Update VueTorrent',
-      update_cron,
-      async () => {
-        return await checkForUpdate()
-      }
-    );
-  }
-})
+const sslOptions = getSSLOptionsFromEnv()
+let server
+if (sslOptions) {
+  const httpsServer = https.createServer(sslOptions, app)
+  server = httpsServer
+  launchServer(httpsServer, 'HTTPS')
+} else {
+  server = app
+  launchServer(app, 'HTTP')
+}
 
 async function stopServer(signal) {
   console.log(`Received ${ signal } signal. Gracefully shutting down...`)
